d51d998f5502ddfe87dbeef663eee9687d42108d
Nicorenox <100243743+Nicorenox@users.noreply.github.com>
2024-10-15 23:00:55 -0500

    Update cliente.py

--- Raft/client/cliente.py
+++ Raft/client/cliente.py
@@ -1,22 +1,38 @@
 import grpc
 import logging
-from raft_pb2 import PutDataRequest, GetDataRequest
-from raft_pb2_grpc import RaftServiceStub
+from concurrent import futures
+import raft_pb2
+import raft_pb2_grpc
 
 # Configurar el logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def main():
-    with grpc.insecure_channel('proxy:50050') as channel:
-        stub = RaftServiceStub(channel)
-        
-        # Ejemplo de escritura
-        put_response = stub.PutData(PutDataRequest(key='key1', data='value1'))
-        logging.info("Respuesta de PutData: %s", put_response.success)
+class RaftProxyServicer(raft_pb2_grpc.RaftServiceServicer):
 
-        # Ejemplo de lectura
-        get_response = stub.GetData(GetDataRequest(key='key1'))
-        logging.info("Respuesta de GetData: %s", get_response.value)
+    def __init__(self, leader_ip, follower_ips):
+        self.leader_ip = leader_ip
+        self.follower_ips = follower_ips
+        logging.info("Cliente inicializado con líder: %s y seguidores: %s", leader_ip, follower_ips)
+
+    def GetData(self, request, context):
+        logging.info("Solicitud de lectura recibida, redirigiendo al seguidor.")
+        follower_channel = grpc.insecure_channel(self.follower_ips[0] + ':50051')  # Asume follower1
+        follower_stub = raft_pb2_grpc.RaftServiceStub(follower_channel)
+        return follower_stub.GetData(request)
+
+    def PutData(self, request, context):
+        logging.info("Solicitud de escritura recibida, redirigiendo al líder.")
+        leader_channel = grpc.insecure_channel(self.leader_ip + ':50050')
+        leader_stub = raft_pb2_grpc.RaftServiceStub(leader_channel)
+        return leader_stub.PutData(request)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    raft_pb2_grpc.add_RaftServiceServicer_to_server(RaftProxyServicer('leader_ip', ['follower1_ip', 'follower2_ip']), server)
+    server.add_insecure_port('[::]:50050')
+    server.start()
+    logging.info("Servidor cliente/proxy iniciado en el puerto 50050")
+    server.wait_for_termination()
 
 if __name__ == '__main__':
-    main()
+    serve()
